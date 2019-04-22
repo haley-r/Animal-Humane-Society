abc83bfb2a76efc5f755f08bded99ec2f8ba9228
haley-r <49448734+haley-r@users.noreply.github.com>
2019-04-22 03:39:36 -0500

    email form popup

--- js/main.js
+++ js/main.js
@@ -31,4 +31,44 @@ navButton.addEventListener('click', function(){
 });
 
 
+var popUp = document.querySelector('.first .more-info .button');
+
+popUp.addEventListener('click', function(){
+    document.querySelector('form')
+      .classList
+      .toggle('display-email-form')
+});
+
+var popUp = document.querySelector('.second .more-info .button');
+
+popUp.addEventListener('click', function(){
+    document.querySelector('form')
+      .classList
+      .toggle('display-email-form')
+});
+
+var popUp = document.querySelector('.third .more-info .button');
+
+popUp.addEventListener('click', function(){
+    document.querySelector('form')
+      .classList
+      .toggle('display-email-form')
+});
+
+var popUp = document.querySelector('.fourth .more-info .button');
+
+popUp.addEventListener('click', function(){
+    document.querySelector('form')
+      .classList
+      .toggle('display-email-form')
+});
+var closePopUp = document.querySelector('form .button');
+
+closePopUp.addEventListener('click', function(){
+    document.querySelector('form')
+      .classList
+      .toggle('display-email-form')
+});
+
+
 
